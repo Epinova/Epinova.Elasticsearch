90c287de04d24ec6d39f6199fd27f30f6d4d38ca
Karl Solgård <karl.f91@gmail.com>
2018-11-01 21:34:00 +0100

    Testing and further implementation

--- src/Epinova.ElasticSearch.Core/Conventions/MessageHandlers.cs
+++ src/Epinova.ElasticSearch.Core/Conventions/MessageHandlers.cs
@@ -1,15 +1,18 @@
-﻿
-using System.Net.Http;
+﻿using System.Net.Http;
 
 namespace Epinova.ElasticSearch.Core.Conventions
 {
+    /// <summary>
+    /// This implementation contains a single instance of <see cref="HttpMessageHandler" />.
+    /// If you want several <see cref="HttpMessageHandler" />s, we recommend chaining them before including.
+    /// </summary>
     public class MessageHandlers
     {
-        public static MessageHandlers Instance { get; } = new MessageHandlers();
+        public static MessageHandlers Instance = new MessageHandlers();
 
-        internal static DelegatingHandler Handler;
+        internal static HttpMessageHandler Handler;
 
-        public MessageHandlers IncludeMessageHandler(DelegatingHandler handler)
+        public MessageHandlers IncludeMessageHandler(HttpMessageHandler handler)
         {
             Handler = handler;
             return this;
---
+++ tests/Core.Tests/Conventions/MessageHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using Epinova.ElasticSearch.Core.Conventions;
+using TestData;
+using Xunit;
+
+namespace Core.Tests.Conventions
+{
+    public class MessageHandlerTests
+    {
+        public MessageHandlerTests()
+        {
+            Factory.SetupServiceLocator();
+        }
+
+        [Fact]
+        public void ExcludeType_AnyType_AddsToCollection()
+        {
+            MessageHandlers.Instance.IncludeMessageHandler(new MockHandler());
+
+            HttpMessageHandler result = MessageHandlers.Handler;
+
+            Assert.IsType<MockHandler>(result);
+        }
+
+        private class MockHandler : DelegatingHandler
+        {
+
+        }
+    }
+}
