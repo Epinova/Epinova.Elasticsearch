4a1218a36b352c024809cc9cf96b9763234e7666
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2019-05-07 13:30:48 +0200

    Refactor controllers

--- src/Epinova.ElasticSearch.Core.EPiServer/Controllers/ElasticAutoSuggestController.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Controllers/ElasticAutoSuggestController.cs
@@ -1,43 +1,40 @@
 ﻿using System;
-using System.Linq;
 using System.Web.Mvc;
 using Epinova.ElasticSearch.Core.EPiServer.Contracts;
 using Epinova.ElasticSearch.Core.EPiServer.Controllers.Abstractions;
 using Epinova.ElasticSearch.Core.EPiServer.Models.ViewModels;
+using Epinova.ElasticSearch.Core.Settings;
 using EPiServer.DataAbstraction;
 
 namespace Epinova.ElasticSearch.Core.EPiServer.Controllers
 {
     public class ElasticAutoSuggestController : ElasticSearchControllerBase
     {
-        private readonly ILanguageBranchRepository _languageBranchRepository;
         private readonly IAutoSuggestRepository _autoSuggestRepository;
 
-        public ElasticAutoSuggestController(ILanguageBranchRepository languageBranchRepository, IAutoSuggestRepository autoSuggestRepository)
+        public ElasticAutoSuggestController(
+            ILanguageBranchRepository languageBranchRepository, 
+            IAutoSuggestRepository autoSuggestRepository,
+            IElasticSearchSettings settings) : base(settings, languageBranchRepository)
         {
-            _languageBranchRepository = languageBranchRepository;
             _autoSuggestRepository = autoSuggestRepository;
         }
 
         [Authorize(Roles = RoleNames.ElasticsearchAdmins)]
         public ActionResult Index(string languageId = null)
         {
-            var languages = _languageBranchRepository.ListEnabled()
-                .Select(lang => new {lang.LanguageID, lang.Name})
-                .ToArray();
-
             AutoSuggestViewModel model = new AutoSuggestViewModel(languageId);
 
-            foreach (var language in languages)
+            foreach (var language in Languages)
             {
-                var name = language.Name;
+                var name = language.Value;
                 name = String.Concat(name.Substring(0, 1).ToUpper(), name.Substring(1));
 
                 model.WordsByLanguage.Add(new LanguageAutoSuggestWords
                 {
                     LanguageName = name,
-                    LanguageId = language.LanguageID,
-                    Words = _autoSuggestRepository.GetWords(language.LanguageID)
+                    LanguageId = language.Key,
+                    Words = _autoSuggestRepository.GetWords(language.Key)
                 });
             }
 
