61b90d57c1f6b8b26b8badeb6e4d4ba7e7c2e207
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2019-05-07 13:34:49 +0200

    Refactor viewmodels

---
+++ src/Epinova.ElasticSearch.Core.EPiServer/Models/ViewModels/TrackingLanguage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Epinova.ElasticSearch.Core.EPiServer.Models.ViewModels.Abstractions;
+
+namespace Epinova.ElasticSearch.Core.EPiServer.Models.ViewModels
+{
+    public class TrackingLanguage : LanguageViewModelBase
+    {
+        public Dictionary<string, long> Searches { get; internal set; } = new Dictionary<string, long>();
+
+        public Dictionary<string, long> SearchesWithoutHits { get; internal set; } = new Dictionary<string, long>();
+    }
+}
--- src/Epinova.ElasticSearch.Core.EPiServer/Models/ViewModels/TrackingViewModel.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Models/ViewModels/TrackingViewModel.cs
@@ -7,12 +7,20 @@ namespace Epinova.ElasticSearch.Core.EPiServer.Models.ViewModels
     {
         public TrackingViewModel(string currentLanguage) : base(currentLanguage)
         {
-            SearchesByLanguage = new List<TrackingByLanguage>();
-            SearchesWithoutHitsByLanguage = new List<TrackingByLanguage>();
         }
 
-        public List<TrackingByLanguage> SearchesByLanguage { get; }
+        public List<TrackingLanguage> Languages { get; } = new List<TrackingLanguage>();
 
-        public List<TrackingByLanguage> SearchesWithoutHitsByLanguage { get; }
+        public void AddLanguage(string name, string id, Dictionary<string, string> indices, Dictionary<string, long> searches, Dictionary<string, long> searchesWithoutHits)
+        {
+            Languages.Add(new TrackingLanguage
+            {
+                LanguageId = id,
+                LanguageName = name,
+                Indices = indices,
+                Searches = searches,
+                SearchesWithoutHits = searchesWithoutHits
+            });
+        }
     }
 }
