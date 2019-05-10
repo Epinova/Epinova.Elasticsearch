7768c9ba5548dcc20125d0eda7ecd1a18b994c9d
Espen Lønnkvist <espen.lonnkvist@epinova.no>
2019-05-10 15:36:06 +0200

    Customresults source fields (#47)
    * Whitespace cleanup
    
    * Clean up sourcefield/returnfields confusion and respect when set to null

--- src/Epinova.ElasticSearch.Core/Models/QuerySetup.cs
+++ src/Epinova.ElasticSearch.Core/Models/QuerySetup.cs
@@ -26,7 +26,6 @@ namespace Epinova.ElasticSearch.Core.Models
             UsePostfilters = true;
         }
 
-
         public Type Type { get; set; }
         public CultureInfo Language { get; set; }
         public List<string> SearchFields { get; set; }
@@ -55,7 +54,7 @@ namespace Epinova.ElasticSearch.Core.Models
         public Dictionary<string, FilterGroupQuery> FilterGroups { get; set; }
         public Type SearchType { get; set; }
         public List<RangeBase> Ranges { get; set; }
-        public string[] ReturnFields { get; set; }
+        public string[] SourceFields { get; set; }
         public string FuzzyLength { get; set; }
         public string IndexName { get; set; }
         public List<Gauss> Gauss { get; set; }
