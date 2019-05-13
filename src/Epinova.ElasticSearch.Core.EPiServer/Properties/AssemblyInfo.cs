b5a03c4c7071446827f24598acc9a44253fe422b
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2019-05-13 16:03:41 +0200

    Create pre-release nuget from vnext branch

--- src/Epinova.ElasticSearch.Core.EPiServer/Properties/AssemblyInfo.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Properties/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("f5a0a67a-4a0f-4a91-b54d-b3fcb1410765")]
 [assembly: AssemblyVersion("11.6.0.0")]
 [assembly: AssemblyFileVersion("11.6.0.0")]
+[assembly: AssemblyInformationalVersion("11.6.0.0")]
 [assembly: InternalsVisibleTo("Epinova.ElasticSearch.Core.EPiServer.Commerce")]
 [assembly: InternalsVisibleTo("Core.EPiServer.Tests")]
 [assembly: InternalsVisibleTo("Integration.Tests")]
