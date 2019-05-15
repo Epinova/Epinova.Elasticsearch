f4dfb2b2467683f2f3b9c4d30956e8e79030cf84
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2019-05-15 15:47:53 +0200

    Fire index update based on transition status

--- src/Epinova.ElasticSearch.Core.EPiServer/Events/IndexingEvents.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Events/IndexingEvents.cs
@@ -2,6 +2,7 @@
 using EPiServer.Logging;
 using EPiServer;
 using EPiServer.Core;
+using EPiServer.DataAccess;
 using EPiServer.ServiceLocation;
 
 namespace Epinova.ElasticSearch.Core.EPiServer.Events
@@ -10,6 +11,7 @@ namespace Epinova.ElasticSearch.Core.EPiServer.Events
     {
         private static readonly ILogger Logger = LogManager.GetLogger(typeof(IndexingEvents));
         private static readonly IIndexer EPiIndexer = ServiceLocator.Current.GetInstance<IIndexer>();
+        private static readonly IContentVersionRepository VersionRepository = ServiceLocator.Current.GetInstance<IContentVersionRepository>();
 
         internal static void DeleteFromIndex(object sender, ContentEventArgs e)
         {
@@ -22,15 +24,54 @@ namespace Epinova.ElasticSearch.Core.EPiServer.Events
 
         internal static void UpdateIndex(object sender, ContentEventArgs e)
         {
+            Logger.Debug($"Raising event UpdateIndex for '{e.Content?.ContentLink}'");
+
             if (ContentReference.WasteBasket.CompareToIgnoreWorkID(e.TargetLink))
             {
                 DeleteFromIndex(sender, e);
                 return;
             }
 
-            Logger.Debug($"Raising event UpdateIndex for '{e.Content.ContentLink}'");
+            var saveArgs = e as SaveContentEventArgs;
+            if (saveArgs == null)
+                return;
 
-            EPiIndexer.Update(e.Content);
+            // Publish
+            if (saveArgs.MaskedAction == SaveAction.Publish && saveArgs.Transition.NextStatus == VersionStatus.Published)
+            {
+                Logger.Debug("Publish-event, update index");
+                EPiIndexer.Update(e.Content);
+                return;
+            }
+
+            // Published => CheckedOut
+            if (saveArgs.Transition.CurrentStatus == VersionStatus.Published && saveArgs.Transition.NextStatus == VersionStatus.CheckedOut)
+            {
+                Logger.Debug("Save-event, previously published, do nothing");
+                return;
+            }
+
+            // CheckedOut => CheckedOut
+            if (saveArgs.Transition.CurrentStatus == VersionStatus.CheckedOut && saveArgs.Transition.NextStatus == VersionStatus.CheckedOut)
+            {
+                var published = VersionRepository.LoadPublished(e.ContentLink);
+                if (published == null)
+                {
+                    Logger.Debug("Save-event, previously unpublished, update index");
+                    EPiIndexer.Update(e.Content);
+                    return;
+                }
+
+                Logger.Debug("Save-event, previously published, do nothing");
+                return;
+            }
+
+            // Create
+            if (saveArgs.Transition.CurrentStatus == VersionStatus.NotCreated)
+            {
+                Logger.Debug("Create-event, do nothing");
+                return;
+            }
         }
     }
 }
