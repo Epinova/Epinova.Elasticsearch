70da5981df7379710da39af14fe16675d08ef378
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2019-06-15 22:29:56 +0200

    Add OnBeforeIndexContent event

--- src/Epinova.ElasticSearch.Core/Events/Events.cs
+++ src/Epinova.ElasticSearch.Core/Events/Events.cs
@@ -1,5 +1,6 @@
 ﻿namespace Epinova.ElasticSearch.Core.Events
 {
+    public delegate void OnBeforeIndexContent(System.EventArgs e);
     public delegate void OnBeforeUpdateItem(IndexItemEventArgs e);
     public delegate void OnAfterUpdateBestBet(BestBetEventArgs e);
 }
