12c565be79616e9db90565c65bc305923f874f00
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2019-06-25 12:23:02 +0200

    Add tests for CoreIndexer

---
+++ tests/Core.Tests/CoreIndexerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Epinova.ElasticSearch.Core;
+using Moq;
+using TestData;
+using Xunit;
+
+namespace Core.Tests
+{
+    [Collection(nameof(ServiceLocatiorCollection))]
+    public class CoreIndexerTests : IClassFixture<ServiceLocatorFixture>
+    {
+        private readonly ServiceLocatorFixture _fixture;
+        private readonly CoreIndexer _coreIndexer;
+
+        public CoreIndexerTests(ServiceLocatorFixture fixture)
+        {
+            _fixture = fixture;
+            _coreIndexer = new CoreIndexer(
+                _fixture.ServiceLocationMock.SettingsMock.Object,
+                _fixture.ServiceLocationMock.HttpClientMock.Object);
+        }
+
+        [Fact]
+        public void Delete_CallsClientHeadAndDelete()
+        {
+            _coreIndexer.Delete("42", "en", typeof(TestPage));
+
+            _fixture.ServiceLocationMock.HttpClientMock.Verify(m => m.Head(It.IsAny<Uri>()), Times.Once);
+            _fixture.ServiceLocationMock.HttpClientMock.Verify(m => m.Delete(It.IsAny<Uri>()), Times.Once);
+        }
+    }
+}
