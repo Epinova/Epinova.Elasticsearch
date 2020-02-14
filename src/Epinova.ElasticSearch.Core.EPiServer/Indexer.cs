03964e38e471fbad30023df945eef0f4f5394897
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2020-02-14 22:46:05 +0100

    Change != false to == true in IsExcludedType, add test for it

--- src/Epinova.ElasticSearch.Core.EPiServer/Indexer.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Indexer.cs
@@ -190,7 +190,7 @@ namespace Epinova.ElasticSearch.Core.EPiServer
 
         private static bool IsExcludedType(Type type)
         {
-            if(type?.Namespace?.StartsWith("Epinova.ElasticSearch", StringComparison.OrdinalIgnoreCase) != false)
+            if(type?.Namespace?.StartsWith("Epinova.ElasticSearch", StringComparison.OrdinalIgnoreCase) == true)
             {
                 return true;
             }
