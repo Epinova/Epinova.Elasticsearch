42c575dae91ce8d213fda883144b14fa3071a2aa
Tore Wiik Gjerdrum <tore.gjerdrum@epinova.no>
2021-05-28 10:07:16 +0200

    Added file provider that will search on all languages

---
+++ src/Epinova.ElasticSearch.Core.EPiServer/Providers/FileSearchProviderWithInvariantCulture.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.Shell.Search;
+
+namespace Epinova.ElasticSearch.Core.EPiServer.Providers
+{
+    [SearchProvider]
+    public class FileSearchProviderWithInvariantCulture : SearchProviderBase<MediaData, MediaData, ContentType>
+    {
+        public FileSearchProviderWithInvariantCulture() : base("file")
+        {
+            IconClass = ProviderConstants.FileIconCssClass;
+            AreaName = ProviderConstants.FileArea;
+        }
+
+        protected override CultureInfo GetLanguage() => CultureInfo.InvariantCulture;
+    }
+}
