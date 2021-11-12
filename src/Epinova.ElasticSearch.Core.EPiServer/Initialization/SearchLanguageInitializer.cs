9348b42a80a0738a06fbd66c77c4e54e31685cb7
NCG\oyvind.tanum <oyvind.tanum@epinova.no>
2021-11-12 14:40:28 +0100

    Temp added

--- src/Epinova.ElasticSearch.Core.EPiServer/Initialization/SearchLanguageInitializer.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Initialization/SearchLanguageInitializer.cs
@@ -1,64 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EPiServer.Framework;
-using EPiServer.ServiceLocation;
+﻿//using Epinova.ElasticSearch.Core.Contracts;
+//using EPiServer.Framework;
+//using EPiServer.Framework.Initialization;
+//using EPiServer.ServiceLocation;
+//using StructureMap;
 
-namespace Epinova.ElasticSearch.Core.EPiServer.Initialization
-{
-    [InitializableModule]
-    [ModuleDependency(typeof(FrameworkInitialization))]
-    public class DependencyResolverInitialization : IConfigurableModule
-    {
-        public void ConfigureContainer(ServiceConfigurationContext context)
-        {
-            context.StructureMap().Configure(ConfigureContainer);
+//namespace Epinova.ElasticSearch.Core.EPiServer.Initialization
+//{
+//    [InitializableModule]
+//    [ModuleDependency(typeof(FrameworkInitialization))]
+//    public class DependencyResolverInitialization : IConfigurableModule
+//    {
+//        public void ConfigureContainer(ServiceConfigurationContext context)
+//        {
+//            context.StructureMap().Configure(ConfigureContainer);
 
 
-            context.ConfigurationComplete += (sender, args) =>
-            {
-                //Locate the specific implementation of IInterface we would like to eject
-                InstanceRef instanceRef = args.StructureMap().Model.For<IInterface>().Instances
-                    .FirstOrDefault(c => c.ReturnedType.Equals(typeof(OptimizelyImplementationOfAbstraction)));
+//            context.ConfigurationComplete += (sender, args) =>
+//            {
+//                args.StructureMap().Model.For<ISearchLanguage>().EjectAndRemove();
 
 
-                if(instanceRef != null)
-                    //Eject it
-                    args.StructureMap().Model.For<IInterface>().EjectAndRemove(instanceRef);
+//                args.StructureMap().Configure(ConfigureContainerAfter);
+//            };
+//        }
 
 
-                args.StructureMap().Configure(ConfigureContainerAfter);
-            };
-        }
+//        private static void ConfigureContainer(ConfigurationExpression container)
+//        {
+//            //Register custom
+//        }
 
 
-        private static void ConfigureContainer(ConfigurationExpression container)
-        {
-            //Register custom
-        }
+//        private static void ConfigureContainerAfter(ConfigurationExpression container)
+//        {
+//            //Register our own to replace what we ejected
+//            container.For<ISearchLanguage>().Add<EpiserverSearchLanguage>();
+//        }
 
 
-        private static void ConfigureContainerAfter(ConfigurationExpression container)
-        {
-            //Register our own to replace what we ejected
-            container.For<IInterface>().Add<MyImplementation>();
-        }
+//        public void Initialize(InitializationEngine context)
+//        {
+//        }
 
 
-        public void Initialize(InitializationEngine context)
-        {
-        }
+//        public void Uninitialize(InitializationEngine context)
+//        {
+//        }
 
-
-        public void Uninitialize(InitializationEngine context)
-        {
-        }
-
-
-        public void Preload(string[] parameters)
-        {
-        }
-    }
-}
+//        public void Preload(string[] parameters)
+//        {
+//        }
+//    }
+//}
