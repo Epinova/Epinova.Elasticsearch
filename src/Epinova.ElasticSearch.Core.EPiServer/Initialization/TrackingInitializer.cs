80f55d084d95e2492791ddd351f41f288d8ea1b9
NCG\oyvind.tanum <oyvind.tanum@epinova.no>
2022-05-16 15:12:17 +0200

    Fixed nullable and primary keys

--- src/Epinova.ElasticSearch.Core.EPiServer/Initialization/TrackingInitializer.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer/Initialization/TrackingInitializer.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using Epinova.ElasticSearch.Core.Utilities;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
@@ -26,9 +29,26 @@ namespace Epinova.ElasticSearch.Core.EPiServer.Initialization
             if(!DbHelper.ColumnExists(connectionString, Constants.Tracking.TableName, Constants.TrackingFieldIndex))
             {
                 Logger.Information($"Extending table with {Constants.TrackingFieldIndex} column");
+                DbHelper.ExecuteCommand(connectionString, $"ALTER TABLE {Constants.Tracking.TableName} ADD {Constants.TrackingFieldIndex} nvarchar(200) NOT NULL");
+            }
+            else if(DbHelper.ColumnIsNullable(connectionString, Constants.Tracking.TableName, Constants.TrackingFieldIndex))
+            {
+                DbHelper.AdjustColumnNullable(connectionString, Constants.Tracking.TableName, Constants.TrackingFieldIndex, "nvarchar(200)", isNullable: false);
+            }
 
-                DbHelper.ExecuteCommand(connectionString, $"ALTER TABLE {Constants.Tracking.TableName} ADD {Constants.TrackingFieldIndex} nvarchar(200)");
-                DbHelper.ExecuteCommand(connectionString, $"IF (OBJECT_ID('PK_ElasticTracking', 'U') IS NOT NULL) BEGIN ALTER TABLE {Constants.Tracking.TableName}  DROP CONSTRAINT [PK_ElasticTracking] END");
+            FixPrimaryKeys(connectionString);
+        }
+
+        private void FixPrimaryKeys(string connectionString)
+        {
+            string primaryKeySql = $"SELECT c.COLUMN_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS t JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE c ON c.CONSTRAINT_NAME= t.CONSTRAINT_NAME WHERE c.TABLE_NAME='{Constants.Tracking.TableName}' and t.CONSTRAINT_TYPE='PRIMARY KEY'";
+            IEnumerable<string> primaryKeyColumns = DbHelper.ExecuteReader(connectionString, primaryKeySql)?.Select(r => Convert.ToString(r["COLUMN_NAME"])) ?? Enumerable.Empty<string>();
+            IEnumerable<string> missingPrimaryKeys = new List<string> { "Query", "Language", "IndexName" }.Except(primaryKeyColumns);
+            
+            if(missingPrimaryKeys.Any())
+            {
+                DbHelper.ExecuteCommand(connectionString, $"ALTER TABLE {Constants.Tracking.TableName} DROP CONSTRAINT[PK_ElasticTracking]");
+                DbHelper.ExecuteCommand(connectionString, $"ALTER TABLE {Constants.Tracking.TableName} ADD CONSTRAINT[PK_ElasticTracking] PRIMARY KEY([Query], [Language], [IndexName])");
             }
         }
 
