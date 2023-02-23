36dc4761dc2ea5aeacc8988c2cd3cc7a7f88212f
NCG\oyvind.tanum <oyvind.tanum@epinova.no>
2023-02-23 08:43:45 +0100

    Converted project to sdk format. Deleted files.

--- src/Epinova.ElasticSearch.Core.EPiServer.Commerce/Properties/AssemblyInfo.cs
+++ src/Epinova.ElasticSearch.Core.EPiServer.Commerce/Properties/AssemblyInfo.cs
@@ -1,16 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Epinova.ElasticSearch.Core.EPiServer.Commerce")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Epinova AS")]
-[assembly: AssemblyProduct("Epinova.ElasticSearch.Core.EPiServer.Commerce")]
-[assembly: AssemblyCopyright("Copyright© 2022 - Epinova AS")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f5a0a67a-4a0f-4a91-b54d-b3fcb1410765")]
-[assembly: AssemblyVersion("11.7.4.0")]
-[assembly: AssemblyFileVersion("11.7.4.0")]
-[assembly: AssemblyInformationalVersion("11.7.4.0")]
--- src/Epinova.ElasticSearch.Core/Properties/AssemblyInfo.cs
+++ src/Epinova.ElasticSearch.Core/Properties/AssemblyInfo.cs
@@ -1,20 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Epinova.ElasticSearch.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Epinova AS")]
-[assembly: AssemblyProduct("Epinova.ElasticSearch.Core")]
-[assembly: AssemblyCopyright("Copyright© 2022 - Epinova AS")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0818a108-083d-438b-9183-aef12733618f")]
-[assembly: AssemblyVersion("11.7.4.0")]
-[assembly: AssemblyFileVersion("11.7.4.0")]
-[assembly: AssemblyInformationalVersion("11.7.4.0")]
 [assembly: InternalsVisibleTo("Epinova.ElasticSearch.Core.EPiServer")]
 [assembly: InternalsVisibleTo("Epinova.ElasticSearch.Core.EPiServer.Commerce")]
 [assembly: InternalsVisibleTo("Core.EPiServer.Tests")]
