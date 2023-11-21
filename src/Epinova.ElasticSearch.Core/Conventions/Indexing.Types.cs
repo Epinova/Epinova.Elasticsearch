d6814e0048c803af53b1f6f0af58a2f85ac0c053
Øyvind Tanum <oyvind.tanum@epinova.no>
2023-11-21 14:57:27 +0100

    Added possibility to include type

--- src/Epinova.ElasticSearch.Core/Conventions/Indexing.Types.cs
+++ src/Epinova.ElasticSearch.Core/Conventions/Indexing.Types.cs
@@ -7,8 +7,10 @@ namespace Epinova.ElasticSearch.Core.Conventions
     public sealed partial class Indexing
     {
         private static readonly List<Type> _listExcludedTypes = new List<Type>();
+        private static readonly List<Type> _listIncludedTypes = new List<Type>();
 
         internal static Type[] ExcludedTypes => _listExcludedTypes.ToArray();
+        internal static Type[] IncludedTypes => _listIncludedTypes.ToArray();
 
         internal Indexing ExcludeType(Type type)
         {
@@ -21,6 +23,17 @@ namespace Epinova.ElasticSearch.Core.Conventions
             return this;
         }
 
+        internal Indexing IncludeType(Type type)
+        {
+            if(!_listIncludedTypes.Contains(type))
+            {
+                Logger.Information($"Include type: {type.FullName}");
+                _listIncludedTypes.Add(type);
+            }
+
+            return this;
+        }
+
         /// <summary>
         /// Excludes the specified type from the index. 
         /// See also <see cref="Attributes.ExcludeFromSearchAttribute"/>
@@ -28,5 +41,12 @@ namespace Epinova.ElasticSearch.Core.Conventions
         /// <typeparam name="T">The type</typeparam>
         /// <returns>The <see cref="Indexing"/> instance</returns>
         public Indexing ExcludeType<T>() => ExcludeType(typeof(T));
+
+        /// <summary>
+        /// Explicit indexing of type.
+        /// </summary>
+        /// <typeparam name="T">The type</typeparam>
+        /// <returns>The <see cref="Indexing"/> instance</returns>
+        public Indexing IncludeType<T>() => IncludeType(typeof(T));
     }
 }
